6adbbcd7b5f8b68f04ce2967fda02f1b80f47bd9
instructor <instructor@example.com>
2022-08-01 15:14:17 +0900

    update code

--- workspace/java-basic/examples-01/src/Ex04ControlStatement01.java
+++ workspace/java-basic/examples-01/src/Ex04ControlStatement01.java
@@ -4,33 +4,61 @@ public class Ex04ControlStatement01 {
 	public static void main(String[] args) {
 		
 		//1. if statement
-		boolean b = true;
-		// boolean b = false;
-		
-		if (b) {
-			System.out.println("선택적 문장 실행 1"); // b가 true인 경우에만 실행
-			System.out.println("선택적 문장 실행 2"); // b가 true인 경우에만 실행
-		}
-		
-		System.out.println("항상 실행되는 문장 실행");
+//		boolean b = true;
+//		// boolean b = false;
+//		
+//		if (b) {
+//			System.out.println("선택적 문장 실행 1"); // b가 true인 경우에만 실행
+//			System.out.println("선택적 문장 실행 2"); // b가 true인 경우에만 실행
+//		}
+//		
+//		System.out.println("항상 실행되는 문장 실행");
+
+		java.util.Scanner scanner = new java.util.Scanner(System.in);		
 		
 		//2. 사용자입력 (숫자) -> 0보다 크면 "큽니다" 출력,  0보다 작으면 "작습니다" 출력, 0이면 "0입니다" 출력
-		java.util.Scanner scanner = new java.util.Scanner(System.in);
+//		
+//		System.out.print("숫자 입력 : ");
+//		int number = scanner.nextInt();
+//		
+//		if (number > 0) { // number > 0 : number가 0보다 크면 true, 아니면 false
+//			System.out.println("0보다 큰 숫자입니다.");
+//		} else if (number < 0){ 
+//			System.out.println("0보다 작은 숫자입니다.");
+//		} else if (number == 0) {
+//			System.out.println("0입니다.");
+//		}
+
 		
-		System.out.print("숫자 입력 : ");
-		int number = scanner.nextInt();
+		//3. 계산기 만들기
+		//   ㄱ. 변수 3개 만들기 : 숫자(int), 연산자(String), 숫자(int)를 저장하는 변수
+		//   ㄴ. 사용자입력 (숫자) 후 변수에 저장, 사용자입력 (연산자) 후 변수에 저장,  사용자입력 (숫자) 후 변수에 저장
+		//   ㄷ. 연산자에 따라 해당 연산 수행 -> 결과를 변수에 저장
+		//   ㄹ. 연산 결과 출력
 		
-		if (number > 0) { // number > 0 : number가 0보다 크면 true, 아니면 false
-			System.out.println("0보다 큰 숫자입니다.");
-		} else if (number < 0){ 
-			System.out.println("0보다 작은 숫자입니다.");
-		} else if (number == 0) {
-			System.out.println("0입니다.");
-		}
+		//ㄱ, ㄴ 작업 수행
 		
-		scanner.close(); // Scanner는 다 사용하면 닫아야 합니다. ( 반납 )
 		
+
+		scanner.close(); // Scanner는 다 사용하면 닫아야 합니다. ( 반납 )		
 
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
