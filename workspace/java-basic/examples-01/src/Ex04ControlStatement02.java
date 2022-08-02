78975882192f43c98e8c6ea020aff4089ad95ff0
instructor <instructor@example.com>
2022-08-02 09:53:45 +0900

    update code

--- workspace/java-basic/examples-01/src/Ex04ControlStatement02.java
+++ workspace/java-basic/examples-01/src/Ex04ControlStatement02.java
@@ -13,14 +13,26 @@ public class Ex04ControlStatement02 {
 		int iNumber = (int)number2;
 		System.out.println("난수 2 : " + iNumber);
 		
-		// 2. 가위 바위 보 게임
+		// Console 입력 도구 변수 만들기
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
+		
+		// 3. 가위 바위 보 게임
 		//    - (1, 2, 3) 범위의 정수 난수 발생 -> 변수에 저장 : 1 - 가위, 2 - 바위, 3 - 보
 		//    - 사용자 입력 후 변수에 저장 : 1, 2, 3 범위의 정수 : 1 - 가위, 2 - 바위, 3 - 보
 		//    - 테스트 코드 ( 두 숫자 출력 )
+		double n = Math.random() * 3; // 0 <= n < 3
+		int comNumber = (int)(n + 1);
+		
+		System.out.print("가위/바위/보 입력 (가위=1, 바위=2, 보=3) : ");
+		int userNumber = scanner.nextInt();
+		
+		System.out.printf("[COMPUTER : %d][USER : %d]\n", comNumber, userNumber);
+		
 		//    - 승부 계산
 		//    - 출력
 		
 		
+		scanner.close(); // 자원 반납
 		
 
 	}
