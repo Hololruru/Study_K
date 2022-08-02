dd0594fd04f4639a2f2fd2191a01ff49841de95c
instructor <instructor@example.com>
2022-08-02 10:17:44 +0900

    update code

--- workspace/java-basic/examples-01/src/Ex04ControlStatement02.java
+++ workspace/java-basic/examples-01/src/Ex04ControlStatement02.java
@@ -3,15 +3,32 @@ public class Ex04ControlStatement02 {
 
 	public static void main(String[] args) {
 		
-		// 1-1. 난수 발생기 사용
-		double number = Math.random(); // Math.random() : 0 <= n < 1 범위의 난수 발생
-		System.out.println("난수 1 : " + number);
+//		// 1-1. 난수 발생기 사용
+//		double number = Math.random(); // Math.random() : 0 <= n < 1 범위의 난수 발생
+//		System.out.println("난수 1 : " + number);
+//		
+//		// 1-2. 1 ~ 100 범위의 정수형 난수 만들기
+//		double number2 = Math.random() * 99; // 0 ~ 99 (실수)
+//		number2 = number2 + 1; // 1 ~ 100 (실수)
+//		int iNumber = (int)number2;
+//		System.out.println("난수 2 : " + iNumber);
 		
-		// 1-2. 1 ~ 100 범위의 정수형 난수 만들기
-		double number2 = Math.random() * 99; // 0 ~ 99 (실수)
-		number2 = number2 + 1; // 1 ~ 100 (실수)
-		int iNumber = (int)number2;
-		System.out.println("난수 2 : " + iNumber);
+//		// 2. 논리 연산
+//		int a = 10, b = 20;		
+//		// if (a == 10 && b == 20) {
+//		// if (a == 10 && b == 30) {
+//		// if (a == 10 || b == 30) {
+//		if (a == 0 || b == 30) {
+//			System.out.println("2. true");
+//		}
+		
+//		//3. 중첩 선택문
+//		int c = 10, d = 20;
+//		if (c == 10) {
+//			if (d == 20) {
+//				System.out.println("3. true");
+//			} 
+//		}
 		
 		// Console 입력 도구 변수 만들기
 		java.util.Scanner scanner = new java.util.Scanner(System.in);
