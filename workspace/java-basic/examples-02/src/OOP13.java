1fd080bcf3ed4a33b257bf1ab872d5dad048f8b4
instructor <instructor@example.com>
2022-08-10 11:18:27 +0900

    update code

---
+++ workspace/java-basic/examples-02/src/OOP13.java
@@ -0,0 +1,31 @@
+
+class TheShape {
+	public void draw() {
+		System.out.println("TheShape.draw");
+	}
+}
+
+class TheRectangle extends TheShape {
+	public void draw() {
+		System.out.println("TheRectangle.draw");
+	}
+}
+class TheOval extends TheShape {
+	public void draw() {
+		System.out.println("TheOval.draw");
+	}
+}
+class TheLine extends TheShape {
+	public void draw() {
+		System.out.println("TheLine.draw");
+	}
+}
+
+public class OOP13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
