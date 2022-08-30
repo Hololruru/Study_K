0997b57586cf1f8cf619bb3b907cb9df8ee1720e
instructor <instructor@example.com>
2022-08-30 10:20:51 +0900

    update code

--- workspace/db/jdbc-basic/src/Lotto.java
+++ workspace/db/jdbc-basic/src/Lotto.java
@@ -1,14 +1,60 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+// 데이터베이스에 테이블 만들기
+// (winning-numbers.txt 내용을 참고해서 ...)
+//		CREATE TABLE tbl_lotto
+//		(
+//			rnd int not null primary key, -- auto_increment 아님
+//		    lottery_date datetime not null,
+//		    no1 decimal(2, 0) not null, -- no1 numeric(2, 0) not null,
+//		    no2 decimal(2, 0) not null,
+//		    no3 decimal(2, 0) not null,
+//		    no4 decimal(2, 0) not null,
+//		    no5 decimal(2, 0) not null,
+//		    no6 decimal(2, 0) not null,
+//		    bonus decimal(2, 0) not null
+//		);
 
 public class Lotto {
 
 	public static void main(String[] args) {
 		
-		// 데이터베이스에 테이블 만들기
-		// (winning-numbers.txt 내용을 참고해서 ...)
-		
 		// winning-numbers.txt 파일 읽기
 		// 파일의 데이터를 데이터베이스에 저장
+		
+		//1. 파일 읽기
+		FileInputStream fis = null;		// 파일 데이터 읽기 기능 제공
+		InputStreamReader isr = null;	// byte[] -> char[] (String)으로 변환 기능 제공
+		BufferedReader br = null; 		// 한 행 단위로 텍스트 파일 읽기 기능 제공
+		
+		try {
+			fis = new FileInputStream("winning-numbers.csv");
+			isr = new InputStreamReader(fis);
+			br = new BufferedReader(isr);
+			while (true) {
+				String line = br.readLine();
+				if (line == null) break; // EOF ( End of File )
+				
+				System.out.println(line);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			try { fis.close(); } catch (Exception ex) {}
+		}
 
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
---
+++ workspace/db/jdbc-basic/src/LottoDto.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class LottoDto {
+	
+	private int rnd;
+	private Date lotteryDate;
+	private int no1;
+	private int no2;
+	private int no3;
+	private int no4;
+	private int no5;
+	private int no6;
+	private int bonus;
+
+}
