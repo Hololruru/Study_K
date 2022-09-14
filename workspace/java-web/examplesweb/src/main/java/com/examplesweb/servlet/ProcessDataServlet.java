6e038d0b722b1c14773f21319094677138075c0b
instructor <instructor@example.com>
2022-09-14 11:09:57 +0900

    update code

--- workspace/java-web/examplesweb/src/main/java/com/examplesweb/servlet/ProcessDataServlet.java
+++ workspace/java-web/examplesweb/src/main/java/com/examplesweb/servlet/ProcessDataServlet.java
@@ -24,10 +24,12 @@ public class ProcessDataServlet extends HttpServlet {
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 요청 데이터 읽기
+		request.setCharacterEncoding("utf-8"); // 요청 데이터의 한글 설정 ( Post 인 경우 )
 		String name = request.getParameter("name");
 		String email = request.getParameter("email");
 		System.out.println(name + " / " + email);
 		
+		response.setContentType("text/html;charset=utf-8");// 응답컨텐츠에 대한 정보 설정 ( 한글 설정 포함 )
 		PrintWriter out = response.getWriter();
 		out.println("<h1>" + name + " / " + email + "</h1>");
 	}
