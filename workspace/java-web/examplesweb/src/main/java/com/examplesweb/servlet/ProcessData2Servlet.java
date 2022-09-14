60662dd204f6e1aacd2108c18f670647a1fac3b6
instructor <instructor@example.com>
2022-09-14 12:00:50 +0900

    update code

--- workspace/java-web/examplesweb/src/main/java/com/examplesweb/servlet/ProcessData2Servlet.java
+++ workspace/java-web/examplesweb/src/main/java/com/examplesweb/servlet/ProcessData2Servlet.java
@@ -1,6 +1,7 @@
 package com.examplesweb.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,6 +19,23 @@ public class ProcessData2Servlet extends HttpServlet {
 		
 		String sDan = req.getParameter("dan"); // 요청 데이터는 언제나 String
 		int dan = Integer.parseInt(sDan); // 문자열 -> 정수
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.printf("<title>구구단(%d)</title>\n", dan);
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<table border='1' style='width:300px;margin:0 auto'>");
+		out.println("<tr>");
+		for (int i = 1; i < 10; i++) {
+			out.printf("<th>%d x %d = %2d</th>\n", dan, i, dan * i);
+		}
+		out.println("</tr>");
+		out.println("</table>");
+		out.println("</body>");
+		out.println("</html>");
 		
 	}
 
