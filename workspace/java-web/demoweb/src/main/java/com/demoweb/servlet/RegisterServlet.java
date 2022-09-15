80a5a6c99cf753d8e97998af80a14bc0326a8701
instructor <instructor@example.com>
2022-09-15 14:58:57 +0900

    update code

---
+++ workspace/java-web/demoweb/src/main/java/com/demoweb/servlet/RegisterServlet.java
@@ -0,0 +1,30 @@
+package com.demoweb.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = { "/account/login.action" })
+public class RegisterServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//1. 요청 처리
+		//2. JSP가 읽을 수 있도록 request에 데이터 저장
+		//3. jsp로 forward 이동
+		
+	}
+
+}
+
+
+
+
+
+
+
+
