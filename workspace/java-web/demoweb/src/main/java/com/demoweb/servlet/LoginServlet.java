7d05fa4a7bb331156e044ab92716e744bef0639d
instructor <instructor@example.com>
2022-09-15 15:39:12 +0900

    update code

--- workspace/java-web/demoweb/src/main/java/com/demoweb/servlet/LoginServlet.java
+++ workspace/java-web/demoweb/src/main/java/com/demoweb/servlet/LoginServlet.java
@@ -9,15 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = { "/account/register.action" })
-public class RegisterServlet extends HttpServlet {
+@WebServlet(urlPatterns = { "/account/login.action" })
+public class LoginServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//1. 요청 처리
 		//2. JSP가 읽을 수 있도록 request에 데이터 저장
 		//3. jsp로 forward 이동
-		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/account/register.jsp");
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/account/login.jsp");
 		rd.forward(req, resp); // 지정된 경로로 forward 이동
 		
 	}
