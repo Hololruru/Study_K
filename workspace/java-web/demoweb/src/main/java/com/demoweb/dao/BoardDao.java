813960b06e013a5bc500eb1711f4da4cbd265487
instructor <instructor@example.com>
2022-09-20 10:02:07 +0900

    update code

---
+++ workspace/java-web/demoweb/src/main/java/com/demoweb/dao/BoardDao.java
@@ -0,0 +1,12 @@
+package com.demoweb.dao;
+
+import com.demoweb.dto.BoardDto;
+
+public class BoardDao {
+	
+	// 사용자가 작성한 게시글 정보를 받아서 데이터베이스에 저장
+	public void insertBoard(BoardDto board) {
+		
+	}
+
+}
---
+++ workspace/java-web/demoweb/src/main/java/com/demoweb/service/BoardService.java
@@ -0,0 +1,14 @@
+package com.demoweb.service;
+
+import com.demoweb.dto.BoardDto;
+
+public class BoardService {
+	
+	// 사용자가 입력한 게시글 데이터를 받아서 글쓰기 처리
+	public void writeBoard(BoardDto board) {
+		
+		
+		
+	}
+
+}
