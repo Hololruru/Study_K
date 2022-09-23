65a7a8d5819699c110fa84e044e11cbde6d25d25
instructor <instructor@example.com>
2022-09-23 10:17:59 +0900

    update code

--- workspace/java-web/demoweb/src/main/java/com/demoweb/servlet/BoardWriteServlet.java
+++ workspace/java-web/demoweb/src/main/java/com/demoweb/servlet/BoardWriteServlet.java
@@ -32,13 +32,13 @@ public class BoardWriteServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// 로그인하지 않은 사용자는 로그인 화면으로 보내기
-		HttpSession session = req.getSession();
-		MemberDto member = (MemberDto)session.getAttribute("loginuser");
-		if (member == null) { // 세션에 로그인 데이터 없음 -> 로그인하지 않은 사용자
-			resp.sendRedirect("/demoweb/account/login.action");
-			return;
-		}
+		// 로그인하지 않은 사용자는 로그인 화면으로 보내기 ( 필터에서 구현하므로 주석 처리 )
+//		HttpSession session = req.getSession();
+//		MemberDto member = (MemberDto)session.getAttribute("loginuser");
+//		if (member == null) { // 세션에 로그인 데이터 없음 -> 로그인하지 않은 사용자
+//			resp.sendRedirect("/demoweb/account/login.action");
+//			return;
+//		}
 		
 		// 1. 요청 처리		
 		
