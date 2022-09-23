34c0a149e5fc1fa18319152525fa07eeedc1933e
instructor <instructor@example.com>
2022-09-23 11:22:05 +0900

    update code

--- workspace/java-web/demoweb/src/main/java/com/demoweb/listener/DemoWebListener.java
+++ workspace/java-web/demoweb/src/main/java/com/demoweb/listener/DemoWebListener.java
@@ -15,10 +15,12 @@ public class DemoWebListener implements ServletContextListener, HttpSessionListe
 
     public void sessionCreated(HttpSessionEvent se)  { 
     	System.out.println("세션 시작");
+    	// application 객체에 접속자 카운트 증가해서 저장
     }
 
     public void sessionDestroyed(HttpSessionEvent se)  { 
     	System.out.println("세션 종료");
+    	// application 객체에 접속자 카운트 감소해서 저장
     }
 
     public void contextDestroyed(ServletContextEvent sce)  { 
