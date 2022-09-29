3f32e650e996fb1a2c9c407a221ee705eb978955
instructor <instructor@example.com>
2022-09-29 13:11:25 +0900

    update code

--- workspace/spring-basic/spring-demoweb-a/src/main/java/com/demoweb/controller/AccountController.java
+++ workspace/spring-basic/spring-demoweb-a/src/main/java/com/demoweb/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.demoweb.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,10 +16,17 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import com.demoweb.dto.MemberDto;
 import com.demoweb.service.AccountService;
+import com.demoweb.service.AccountServiceImpl;
+
+import lombok.Setter;
 
 @Controller
 @RequestMapping(path = { "/account" })
 public class AccountController {
+	
+	@Autowired
+	@Qualifier("accountService")
+	private AccountService accountService;
 	
 	@GetMapping(path = { "/register.action" })
 	public String showRegisterForm() {
@@ -31,7 +40,6 @@ public class AccountController {
 		System.out.println(member);
 		
 		// 2. 요청 처리
-		AccountService accountService = new AccountService();
 		accountService.registerMember(member);
 		
 		// 3. View에서 사용할 수 있도록 데이터 전달
@@ -51,7 +59,6 @@ public class AccountController {
 		// 1. 요청 데이터 읽기 ( 전달인자 사용해서 대체 )
 		
 		// 2. 요청 처리
-		AccountService accountService = new AccountService();
 		MemberDto member = accountService.findMemberByIdAndPasswd(memberId, passwd);
 		
 		if (member != null) {
