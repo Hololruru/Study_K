4c6f0cf175671dcba63e36a8e8b33eb92389f53f
instructor <instructor@example.com>
2022-09-29 16:00:29 +0900

    update code

--- workspace/spring-basic/spring-demoweb-a/src/main/java/com/demoweb/controller/BoardController.java
+++ workspace/spring-basic/spring-demoweb-a/src/main/java/com/demoweb/controller/BoardController.java
@@ -2,6 +2,8 @@ package com.demoweb.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.demoweb.dto.BoardDto;
 import com.demoweb.service.BoardService;
+import com.demoweb.service.BoardServiceImpl;
 
 @Controller
 @RequestMapping(path = { "/board" })
 public class BoardController {
+
+	@Autowired
+	@Qualifier("boardService")
+	private BoardService boardService;
 	
 	@GetMapping(path = { "/list.action" })
 	public String showBoardList(Model model) {
 		// 1. 요청 데이터 읽기
 		// 2. 데이터 처리 ( 데이터 조회 )
-		BoardService boardService = new BoardService();
+		
 		List<BoardDto> boards = boardService.findAllBoard();
 		
 		// 3. View에서 읽을 수 있도록 데이터 저장
