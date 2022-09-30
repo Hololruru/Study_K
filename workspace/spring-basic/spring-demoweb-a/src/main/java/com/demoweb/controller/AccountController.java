95c10f6e349bae753d6b78ff344437f530509983
instructor <instructor@example.com>
2022-09-30 13:23:58 +0900

    update code

--- workspace/spring-basic/spring-demoweb-a/src/main/java/com/demoweb/controller/AccountController.java
+++ workspace/spring-basic/spring-demoweb-a/src/main/java/com/demoweb/controller/AccountController.java
@@ -24,6 +24,7 @@ import lombok.Setter;
 @RequestMapping(path = { "/account" })
 public class AccountController {
 	
+	
 	@Autowired
 	@Qualifier("accountService")
 	private AccountService accountService;
