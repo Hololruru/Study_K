80dd754547d33bb53d932b66d6ffb0f0e9a4732d
instructor <instructor@example.com>
2022-10-05 11:21:42 +0900

    update code

--- workspace/spring-basic/spring-demoweb-b/src/main/java/com/demoweb/dao/MyBatisBoardDaoImpl.java
+++ workspace/spring-basic/spring-demoweb-b/src/main/java/com/demoweb/dao/MyBatisBoardDaoImpl.java
@@ -154,53 +154,8 @@ public class MyBatisBoardDaoImpl implements BoardDao {
 	// 글번호를 받아서 게시글 조회 및 반환 ( primary key 검색이므로 단일 객체 반환 )
 	@Override
 	public BoardDto selectBoardByBoardNo(int boardNo) {
-		Connection conn = null;			// 연결과 관련된 JDBC 호출 규격 ( 인터페이스 )
-		PreparedStatement pstmt = null;	// 명령 실행과 관련된 JDBC 호출 규격 ( 인터페이스 )
-		ResultSet rs = null;			// 결과 처리와 관련된 JDBC 호출 규격 ( 인터페이스 )
 		
-		BoardDto board = null;		// 조회한 데이터를 저장할 DTO 객체
-		
-		try {
-			// 1. Driver 등록
-			// DriverManager.registerDriver(new Driver());
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			// 2. 연결 및 연결 객체 가져오기
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/demoweb",	 	// 데이터베이스 연결 정보
-					"testuser", "mysql"); 						// 데이터베이스 계정 정보
-			
-			// 3. SQL 작성 + 명령 객체 가져오기
-			String sql = 
-					"SELECT boardno, title, writer, content, readcount, regdate " +
-					"FROM board " +
-					"WHERE boardno = ? AND deleted = FALSE ";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, boardNo);
-			
-			// 4. 명령 실행
-			rs = pstmt.executeQuery(); // executeQuery : select 일 때 사용하는 메서드
-						
-			// 5. 결과 처리 (결과가 있다면 - SELECT 명령을 실행한 경우)
-			while (rs.next()) {	// 결과 집합의 다음 행으로 이동
-				board = new BoardDto();
-				board.setBoardNo(rs.getInt(1));
-				board.setTitle(rs.getString(2));
-				board.setWriter(rs.getString(3));
-				board.setContent(rs.getString(4));
-				board.setReadCount(rs.getInt(5));
-				board.setRegDate(rs.getDate(6));
-			}
-			
-		} catch (Exception ex) {
-			ex.printStackTrace(); // 개발 용도로 사용
-		} finally {
-			// 6. 연결 닫기
-			try { rs.close(); } catch (Exception ex) {}
-			try { pstmt.close(); } catch (Exception ex) {}
-			try { conn.close(); } catch (Exception ex) {}
-		}
-		
+		BoardDto board = sqlSession.selectOne(namespace + "selectBoardByBoardNo", boardNo);
 		return board;
 	}
 	
@@ -321,54 +276,9 @@ public class MyBatisBoardDaoImpl implements BoardDao {
 	// 글번호를 받아서 첨부파일 정보 조회하고 목록으로 반환
 	@Override
 	public List<BoardAttachDto> selectBoardAttachByBoardNo(int boardNo) {
-		Connection conn = null;			// 연결과 관련된 JDBC 호출 규격 ( 인터페이스 )
-		PreparedStatement pstmt = null;	// 명령 실행과 관련된 JDBC 호출 규격 ( 인터페이스 )
-		ResultSet rs = null;			// 결과 처리와 관련된 JDBC 호출 규격 ( 인터페이스 )
 		
-		ArrayList<BoardAttachDto> attachments = new ArrayList<>();	// 조회한 데이터를 저장할 DTO 객체
-		
-		try {
-			// 1. Driver 등록
-			// DriverManager.registerDriver(new Driver());
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			// 2. 연결 및 연결 객체 가져오기
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/demoweb",	 	// 데이터베이스 연결 정보
-					"testuser", "mysql"); 						// 데이터베이스 계정 정보
-			
-			// 3. SQL 작성 + 명령 객체 가져오기
-			String sql = 
-					"SELECT attachno, boardno, userfilename, savedfilename, downloadcount " +
-					"FROM boardattach " +
-					"WHERE boardno = ? ";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, boardNo);
-			
-			// 4. 명령 실행
-			rs = pstmt.executeQuery(); // executeQuery : select 일 때 사용하는 메서드
-						
-			// 5. 결과 처리 (결과가 있다면 - SELECT 명령을 실행한 경우)
-			while (rs.next()) {	// 결과 집합의 다음 행으로 이동
-				BoardAttachDto attachment = new BoardAttachDto();
-				attachment.setAttachNo(rs.getInt(1));
-				attachment.setBoardNo(rs.getInt(2));
-				attachment.setUserFileName(rs.getString(3));
-				attachment.setSavedFileName(rs.getString(4));
-				attachment.setDownloadCount(rs.getInt(5));
-				
-				attachments.add(attachment);
-			}
-			
-		} catch (Exception ex) {
-			ex.printStackTrace(); // 개발 용도로 사용
-		} finally {
-			// 6. 연결 닫기
-			try { rs.close(); } catch (Exception ex) {}
-			try { pstmt.close(); } catch (Exception ex) {}
-			try { conn.close(); } catch (Exception ex) {}
-		}
-		
+		List<BoardAttachDto> attachments = 
+				sqlSession.selectList(namespace + "selectBoardAttachByBoardNo", boardNo);		
 		return attachments;
 	}
 	
