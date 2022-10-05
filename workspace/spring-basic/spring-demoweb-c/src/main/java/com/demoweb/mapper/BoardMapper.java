37c642bfafa4ab3ab6e4f08b1e3b9e64e4b59cbc
instructor <instructor@example.com>
2022-10-05 18:22:17 +0900

    update code

--- workspace/spring-basic/spring-demoweb-c/src/main/java/com/demoweb/mapper/BoardMapper.java
+++ workspace/spring-basic/spring-demoweb-c/src/main/java/com/demoweb/mapper/BoardMapper.java
@@ -2,11 +2,13 @@ package com.demoweb.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.demoweb.dto.BoardAttachDto;
 import com.demoweb.dto.BoardDto;
 
+@Mapper // board-context.xml 파일의 <bean id="boardMapper" class="...MapperFactoryBean" ..을 대신하는 annotation
 public interface BoardMapper {
 	
 	void insertBoard(BoardDto board);
