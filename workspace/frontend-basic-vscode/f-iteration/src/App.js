bcbacd52952ce94f636b0fb1d383533e9f508221
instructor <instructor@domain.com>
2022-12-06 10:18:03 +0900

    update code

---
+++ workspace/frontend-basic-vscode/f-iteration/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Actors from './Actors';
+
+function App() {
+  return (
+    <div>
+      <Actors />
+    </div>
+  );
+}
+
+export default App;
