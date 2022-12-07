9822c3ce29289063a2d531263905eb328ff2a8c3
instructor <instructor@domain.com>
2022-12-07 18:11:48 +0900

    update code

--- workspace/frontend-basic-vscode/g-routing/src/App.js
+++ workspace/frontend-basic-vscode/g-routing/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from "./Home";
 import About from "./About";
 import Profile from "./Profile";
+import Search from "./Search";
 
 function App() {
   return (
@@ -13,11 +14,13 @@ function App() {
       [<Link to="/about">About</Link>]
       [<Link to="/profile/johndoe">John Doe's Profile</Link>]
       [<Link to="/profile/janedoe">Jane Doe's Profile</Link>]
+      [<Link to="/search?keyword=react">Search React</Link>]
       <hr />
       <Routes>
         <Route path="/" element={ <Home /> }></Route>
         <Route path="/about" element={ <About /> }></Route>
         <Route path="/profile/:username" element={ <Profile /> }></Route>
+        <Route path="/search" element={ <Search /> }></Route>
       </Routes>
     </div>
   );
