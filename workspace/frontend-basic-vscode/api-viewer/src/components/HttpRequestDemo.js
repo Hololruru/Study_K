b0c705b541394f4d65e152b5a84a8cb15f67e894
instructor <instructor@domain.com>
2022-12-07 12:39:42 +0900

    update code

--- workspace/frontend-basic-vscode/api-viewer/src/components/HttpRequestDemo.js
+++ workspace/frontend-basic-vscode/api-viewer/src/components/HttpRequestDemo.js
@@ -6,16 +6,21 @@ const HttpRequestDemo = (props) => {
     const [data, setData] = useState(null);
 
     const clickHandler1 = (e) => {
-        axios.get("https://jsonplaceholder.typicode.com/todos/2")
+        axios.get("https://jsonplaceholder.typicode.com/todos/1")
              .then( (response) => {
                 setData(response.data);
              });
     };
 
+    const clickHandler2 = async (e) => {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/todos/2");
+        setData(response.data);
+    };
+
     return (
         <div>
             <button onClick={clickHandler1}>데이터 가져오기 1</button>
-            <button>2</button>
+            <button onClick={clickHandler2}>데이터 가져오기 2</button>
             <hr />
             <div>
                 { data ? JSON.stringify(data) : "버튼을 클릭해서 데이터를 요청하세요" }
