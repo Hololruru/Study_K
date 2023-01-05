1cd6b527b2e403713bd2c70287b514ec741a856f
instructor <instructor@domain.com>
2023-01-05 16:19:51 +0900

    update code

--- workspace/python-workspace/flaskweb1/demo.py
+++ workspace/python-workspace/flaskweb1/demo.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -9,3 +9,19 @@ app = Flask(__name__)
 @app.get("/home")
 def home():
     return render_template("home.html")
+
+@app.get("/request-data-test")
+def process_get_request():
+    email = request.args['email']
+    # passwd = request.args['passwd'] # key가 없으면 key error 발생
+    passwd = request.args.get('passwd', 'password not specified')
+
+    return "GET: Email : {0} / Password : {1}".format(email, passwd)
+
+@app.post("/request-data-test")
+def process_post_request():
+    email = request.form['email']
+    passwd = request.form['passwd']
+
+    return "POST: Email : {0} / Password : {1}".format(email, passwd)
+    pass
