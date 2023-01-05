1e877fc65911f6f54c15b1a2b43302c23a008df1
instructor <instructor@domain.com>
2023-01-05 18:07:35 +0900

    update code

--- workspace/python-workspace/flaskweb1/demo.py
+++ workspace/python-workspace/flaskweb1/demo.py
@@ -24,4 +24,52 @@ def process_post_request():
     passwd = request.form['passwd']
 
     return "POST: Email : {0} / Password : {1}".format(email, passwd)
-    pass
+
+
+@app.post("/game1")
+def game1():
+    user = int(request.form.get('selection', 0))
+
+    if user == 0:
+        return "Invalid User Selection"
+
+    import random
+    com = random.randint(1, 3)
+
+    result = None
+    if (user == 1 and com == 3) or \
+       (user == 2 and com == 1) or \
+       (user == 3 and com == 2):
+       result = 'User Win'
+    elif user == com:
+        result = "Draw"
+    else:
+        result = "Computer Win"
+        
+    return "RESULT : {0}".format(result)
+
+@app.post("/lotto")
+def select_golden_numbers():
+    import random
+    cnt = request.form.get("cnt", 1)
+    try:
+        cnt = int(cnt)
+    except:
+        cnt = 1
+
+    numbers_list = []
+    for rnd in range(cnt):
+        numbers = []
+        while True:
+            number = random.randint(1, 45)
+            if number not in numbers:
+                numbers.append(number)
+            
+            if len(numbers) == 6:
+                break
+                        
+        numbers = sorted(numbers)
+        numbers_list.append(numbers)
+
+    return render_template('lotto-result.html', numbers_list=numbers_list)
+    
