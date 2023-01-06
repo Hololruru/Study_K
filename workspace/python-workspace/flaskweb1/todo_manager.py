0de32902ece850b46a942fd0b5271dbc353909c1
instructor <instructor@domain.com>
2023-01-06 15:07:16 +0900

    update code

--- workspace/python-workspace/flaskweb1/todo_manager.py
+++ workspace/python-workspace/flaskweb1/todo_manager.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
+from todo_dao import TodoDao
 
 app = Flask(__name__)
 
@@ -9,3 +10,28 @@ app = Flask(__name__)
 def home():
 
     return render_template("home2.html")
+
+@app.get("/todo/")
+@app.get("/todo/list")
+def todo_list():
+
+    # 데이터베이스 데이터 조회
+
+    return render_template("todo/todo-list.html")
+
+@app.get("/todo/write")
+def show_todo_write_form():
+
+    return render_template("todo/todo-write.html")
+
+@app.post("/todo/write")
+def todo_write():
+
+    title = request.form.get('title', '')
+    content = request.form.get('content', '')
+
+    todoDao = TodoDao()
+    todoDao.insertTodo( (title, content) )
+
+    # return redirect("/todo/list")
+    return redirect(url_for("todo_list"))
