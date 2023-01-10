a9c45576ccaa6233710c212cc2eba7eec002759f
instructor <instructor@domain.com>
2023-01-10 12:59:50 +0900

    update code

---
+++ workspace/spring-boot-basic/spring-board-a/src/main/java/com/demoweb/config/DatabaseConfig.java
@@ -0,0 +1,36 @@
+package com.demoweb.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration // -> servlet-context.xml 과 같은 설정파일 클래스로 구현
+@PropertySource("classpath:/application.properties")
+public class DatabaseConfig {
+	
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource.hikari")
+	public HikariConfig hikariConfig() {
+		HikariConfig config = new HikariConfig();
+//		config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//		config.setJdbcUrl("jdbc:mysql://localhost:3306/demoweb");
+//		config.setUsername("testuser");
+//		config.setPassword("testuser");
+		
+		return config;
+	}
+	
+	@Bean // --> servlet-context.xml과 같은 설정 파일의 <bean에 해당
+	public DataSource dataSource() {		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig());
+		
+		return dataSource;
+	}
+
+}
