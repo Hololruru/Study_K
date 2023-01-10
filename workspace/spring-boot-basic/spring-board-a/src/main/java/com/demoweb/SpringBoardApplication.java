ca90ce9808c6b1ea441abbb296f140895ae09b65
instructor <instructor@domain.com>
2023-01-10 12:00:55 +0900

    update code

--- workspace/spring-boot-basic/spring-board-a/src/main/java/com/demoweb/SpringBoardApplication.java
+++ workspace/spring-boot-basic/spring-board-a/src/main/java/com/demoweb/SpringBoardApplication.java
@@ -1,4 +1,4 @@
-package com.springexample.springboard;
+package com.demoweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- workspace/spring-boot-basic/spring-board-a/src/main/java/com/demoweb/controller/HomeController.java
+++ workspace/spring-boot-basic/spring-board-a/src/main/java/com/demoweb/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.springexample.springboard.controller;
+package com.demoweb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
