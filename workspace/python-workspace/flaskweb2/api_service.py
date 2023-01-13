ce600e794be86c28b877b05e67fe62eb4b6821a0
instructor <instructor@domain.com>
2023-01-13 16:17:33 +0900

    update code

--- workspace/python-workspace/flaskweb2/api_service.py
+++ workspace/python-workspace/flaskweb2/api_service.py
@@ -1,7 +1,45 @@
 from flask import Flask
+from flask import request, Response
+import json
+
 
 app = Flask(__name__) # web application 
 
 @app.get("/")
 def home():
     return "home"
+
+@app.get("/get-greetings")
+def get_greetings():
+    name = request.args.get("name", None)
+    if name == None:
+        return "Hello, Java !!!!!"
+    else:
+        return "Hello, " + name
+
+def select_winning_numbers():
+    import random
+    numbers = []
+    while True:
+        current_number = int( random.random() * 45 + 1 )
+        isDup = False
+        for number in numbers:
+            if current_number == number:
+                isDup = True
+                break        
+        if not isDup:
+            numbers.append(current_number)
+        if len(numbers) > 6:
+            break
+    numbers.sort()
+    return numbers
+
+@app.get("/get-winning-numbers")
+def get_winning_numbers():
+    import json
+    numbers = select_winning_numbers()
+
+    json_numbers = json.dumps({"선택된 번호" : numbers}, ensure_ascii=False).encode("utf-8")
+    return json_numbers
+    # return Response(json_numbers, mimetype="application/json")
+
--- workspace/spring-boot-basic/spring-boot-example/src/main/java/com/springbootexample/controller/DemoController.java
+++ workspace/spring-boot-basic/spring-boot-example/src/main/java/com/springbootexample/controller/DemoController.java
@@ -1,7 +1,15 @@
 package com.springbootexample.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemoController {
@@ -12,4 +20,82 @@ public class DemoController {
 		return "demo-home";
 	}
 	
+	@GetMapping(path = { "/get-greetings" })
+	@ResponseBody
+	public String getGreetings(String name) {
+	
+		try {
+			String path = "http://localhost:5000/get-greetings" + ( name != null ? "?name=" + URLEncoder.encode(name, "utf-8") : "");
+			URL url = new URL(path);
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			
+			int respCode = conn.getResponseCode();
+			if (respCode == HttpURLConnection.HTTP_OK) {
+				InputStream is = conn.getInputStream();
+				InputStreamReader isr = new InputStreamReader(is, "utf-8");
+				BufferedReader br = new BufferedReader(isr);
+				String message = "";
+				while (true) {
+					String line = br.readLine();
+					if (line == null) {
+						break;
+					}
+					message += line;
+				}
+		
+				return "MESSAGE FROM FLASK : " + message;
+			} else {
+				return "fail to receive data 1";
+			}
+		} catch (Exception ex) {
+			return "fail to receive data 2";
+		}
+		
+	}
+
+
+	@GetMapping(path = { "/get-winning-numbers" })
+	@ResponseBody
+	public String getWinningNumbers() {
+		
+		try {
+			String path = "http://localhost:5000/get-winning-numbers";
+			URL url = new URL(path);
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			
+			int respCode = conn.getResponseCode();
+			if (respCode == HttpURLConnection.HTTP_OK) {
+				InputStream is = conn.getInputStream();
+				InputStreamReader isr = new InputStreamReader(is, "utf-8");
+				BufferedReader br = new BufferedReader(isr);
+				String message = "";
+				while (true) {
+					String line = br.readLine();
+					if (line == null) {
+						break;
+					}
+					message += line;
+				}
+		
+				return message;
+			} else {
+				return "fail to receive data 1";
+			}
+		} catch (Exception ex) {
+			return "fail to receive data 2";
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
